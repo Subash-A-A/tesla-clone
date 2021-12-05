a1ed4a0f0d5b90b21a4d8d21f9eaabeb3b2387e5
Subash <it.subashaa@gmail.com>
2021-12-05 20:16:40 +0530

    commit

---
+++ src/components/Home.js
@@ -0,0 +1,46 @@
+import React from "react";
+import styled from "styled-components";
+import Section from "./Section";
+
+function Home() {
+  return (
+    <Container>
+      <Section
+        title="Model S"
+        description="Order Online for Touchless Delivery"
+        backgroundImage="model-s.jpg"
+        leftButtonText="Custom Order"
+        rightButtonText="Existing Inventory"
+      />
+      <Section
+        title="Model Y"
+        description="Order Online for Touchless Delivery"
+        backgroundImage="model-y.jpg"
+        leftButtonText="Custom Order"
+        rightButtonText="Existing Inventory"
+      />
+      <Section
+        title="Model 3"
+        description="Order Online for Touchless Delivery"
+        backgroundImage="model-3.jpg"
+        leftButtonText="Custom Order"
+        rightButtonText="Existing Inventory"
+      />
+      <Section
+        title="Model X"
+        description="Order Online for Touchless Delivery"
+        backgroundImage="model-x.jpg"
+        leftButtonText="Custom Order"
+        rightButtonText="Existing Inventory"
+      />
+    </Container>
+  );
+}
+
+export default Home;
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-type: y mandatory;
+  overflow-y: scroll;
+  overflow-x: hidden;
+`;
---
+++ src/components/Section.js
@@ -0,0 +1,89 @@
+import React from "react";
+import styled from "styled-components";
+
+function Section({
+  title,
+  description,
+  leftButtonText,
+  rightButtonText,
+  backgroundImage,
+}) {
+  return (
+    <Wrap bgImage={backgroundImage}>
+      <ItemText>
+        <h1>{title}</h1>
+        <p>{description}</p>
+      </ItemText>
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton>{leftButtonText}</LeftButton>
+          <RightButton>{rightButtonText}</RightButton>
+        </ButtonGroup>
+        <DownArrow src="/images/down-arrow.svg"></DownArrow>
+      </Buttons>
+    </Wrap>
+  );
+}
+
+export default Section;
+
+const Wrap = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  background-image: ${(props) => `url("/images/${props.bgImage}")`};
+  scroll-snap-align: start;
+`;
+
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`;
+
+const Buttons = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+  gap: 25px;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+const LeftButton = styled.div`
+  cursor: pointer;
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: bold;
+`;
+
+const RightButton = styled(LeftButton)`
+  background-color: white;
+  opacity: 0.65;
+  color: black;
+`;
+const DownArrow = styled.img`
+  margin-top: 20px;
+  margin-bottom: 20px;
+  height: 40px;
+  animation: bounce infinite 1.5s;
+`;
